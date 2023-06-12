8fcdcbc18bbc50c3e061703f1b5694b5b5bcc93d
Атамас Мария <atamasmari@yandex.ru>
2023-06-12 22:17:31 +0300

    fix:

--- main-service/src/main/java/ru/practicum/ewm/request/repository/RequestRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/request/repository/RequestRepository.java
@@ -1,9 +1,6 @@
 package ru.practicum.ewm.request.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import ru.practicum.ewm.request.dto.EventRequestCountDto;
 import ru.practicum.ewm.request.model.Request;
 import ru.practicum.ewm.request.model.RequestStatus;
 
@@ -12,14 +9,6 @@ import java.util.Optional;
 import java.util.Set;
 
 public interface RequestRepository extends JpaRepository<Request, Long> {
-
-    /*@Query("SELECT NEW ru.practicum.ewm.request.dto.EventRequestCountDto(r.event.id, COUNT(*), r.status) " +
-            "FROM Request as r " +
-            "WHERE (r.event.id IN :eventIds) " +
-            "AND (r.status = :status) " +
-            "GROUP BY r.id")
-    List<EventRequestCountDto> findEventRequest(@Param("eventIds") Set<Long> eventIds,
-                                                @Param("status") RequestStatus status);*/
 
     List<Request> findAllByIdInAndStatus(Set<Long> eventId, RequestStatus status);
 
