ed0eb9fff1d65bfe992fa6ee5419060a2a47d9b9
Атамас Мария <atamasmari@yandex.ru>
2023-06-15 16:40:41 +0300

    fix: 172/36

--- main-service/src/main/java/ru/practicum/ewm/exception/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/ewm/exception/ErrorHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,6 +21,18 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiError handleBadRequestException(final MethodArgumentNotValidException e) {
         log.warn("400 {}", e.getMessage());
+        return ApiError.builder()
+                .message(e.getMessage())
+                .reason(e.toString())
+                .status("BAD_REQUEST")
+                .timestamp(LocalDateTime.now().format(DATE_TIME_FORMATTER))
+                .build();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleMissingServletRequestParameterException(final MissingServletRequestParameterException e) {
+        log.warn("400 {}", e.getMessage(), e);
         return ApiError.builder()
                 .message(e.getMessage())
                 .reason(e.toString())
